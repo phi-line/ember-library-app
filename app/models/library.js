8eb9ba7149adfa7c75706332009c23c0827dcc96
phi- <phi.liney@gmail.com>
2017-06-06 15:47:44 -0700

    lesson 5

--- app/models/library.js
+++ app/models/library.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   addresss: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  isValid: Ember.computed.notEmpty('name')
 });
