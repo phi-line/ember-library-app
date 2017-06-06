a0da78c5796c20ad5672ee1fd30b7f4168d2b4ca
phi- <phi.liney@gmail.com>
2017-06-06 14:17:00 -0700

    lesson 3

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
@@ -9,11 +11,13 @@ export default Ember.Controller.extend({
   actions: {
     saveInvitation() {
       const email = this.get('emailAddress');
-      const newInvitation = this.store.createRecord('invitation', {email: email});
-      newInvitation.save();
 
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      const newInvitation = this.store.createRecord('invitation', {email: email});
+
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+        this.set('emailAddress', '');
+      });
     }
   }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage(newContactMessage) {
+      newContactMessage.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      model.rollbackAttributes();
+      this.controller.set('responseMessage', false);
+    }
+  }
 });
